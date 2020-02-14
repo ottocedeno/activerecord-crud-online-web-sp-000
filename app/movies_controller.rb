1d95cd0e754e9e733416310c7828ac26b41ba2d9
Otto Cedeno <otto@ottostacos.com>
2020-02-14 01:37:38 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -36,7 +36,7 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
 end
 
 def can_get_the_first_item_in_the_database
-  Movie.all.first
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
@@ -48,7 +48,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  
+
 end
 
 def can_find_by_multiple_attributes
