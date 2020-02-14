8165b1bb6e2da7a8892861219a4b35ad937fd4e1
Otto Cedeno <otto@ottostacos.com>
2020-02-14 02:20:24 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -93,7 +93,7 @@ end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
+  Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once").delete
 end
 
 def can_destroy_all_items_at_once
